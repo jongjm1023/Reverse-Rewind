026106c90e03d0b85fc77d0fe1b2ffae7ee466a5
jongjm1023 <jongjm1023@gmail.com>
2026-01-28 19:54:37 +0900

    Fix: 스테이지 최종 수정

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -7,6 +7,14 @@ public class Main : MonoBehaviour
     [Tooltip("Name of the scene to load.")]
     public string targetSceneName;
 
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // 메인 씬으로 돌아왔을 때 커서가 보이도록 설정
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
     public void LoadScene()
     {
         if (!string.IsNullOrEmpty(targetSceneName))
